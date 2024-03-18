b92da15bfb86ec45f2f760c57918610c5d738480
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-18 14:06:15 -0300

    feat: Finish the PricesContainer component, create the Footer component.

--- src/components/home/pricesContainer/PricesContainer.tsx
+++ src/components/home/pricesContainer/PricesContainer.tsx
@@ -4,7 +4,25 @@ export default function PricesContainer() {
 
     return (
         <div className="mt-12 mx-4">
-            <div className="bg-white p-5 rounded-xl">
+            <h2 className="font-semibold text-3xl text-grey_15 mb-1">
+                Our Pricing
+            </h2>
+            <p className="text-sm text-grey_35 mb-5 leading-[150%]">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, voluptas cupiditate consequuntur quidem quos quas dolor natus tempore cum ratione, quaerat iusto voluptate obcaecati soluta fugit exercitationem eius laudantium suscipit!
+            </p>
+            <div className="flex justify-center">
+                <div className="bg-white p-3 mb-10 rounded-lg inline-block">
+                    <div className="flex">
+                        <div className="bg-orange_50 text-white py-3 px-6 rounded-md">
+                            <p className="text-sm font-medium">Monthly</p>
+                        </div>
+                        <div className="text-grey_30 py-3 px-6 rounded-md">
+                            <p className="text-sm font-medium">Yearly</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="bg-white flex flex-col gap-8 p-5 rounded-xl">
                 <PricesCard
                     planCategory="Free"
                     planPrice="0"
