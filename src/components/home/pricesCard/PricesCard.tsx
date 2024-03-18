4accfbccc7b96f37634d6599a2ca9f761874c361
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-18 12:02:03 -0300

    feat: Create the dynamic code for the PricesCard component and test inside the PricesContainer component.

--- src/components/home/pricesCard/PricesCard.tsx
+++ src/components/home/pricesCard/PricesCard.tsx
@@ -1,25 +1,58 @@
 import Link from "next/link";
 
-export default function PricesCard() {
+import { IoMdCheckmark } from "react-icons/io";
+import { FaXmark } from "react-icons/fa6";
+
+type Props = {
+    planCategory: string,
+    planPrice: string,
+    availableFeatures: Array<string>,
+    unavailableFeatures: Array<string>
+}
+
+export default function PricesCard({ planCategory, planPrice, availableFeatures, unavailableFeatures }: Props) {
     return (
         <div className="bg-white_99 border-[1px] border-white_95 rounded-xl">
             <div className="flex flex-col gap-8 py-8 px-5">
                 <h4 className="bg-orange_97 border-[1px] border-orange_90 text-grey_15 text-center font-medium py-2 leading-[169%] rounded-sm">
-                    Free Plan
+                    {planCategory} Plan
                 </h4>
                 <h2 className="text-grey_15 text-5xl text-center font-semibold leading-[73%]">
-                    $0
+                    ${planPrice}
                     <span className="text-sm font-medium">
                         /month
                     </span>
                 </h2>
                 <div className="flex flex-col">
                     <div className="bg-white border-[1px] border-white_95 p-5 rounded-t-md">
-                        <h3 className="text-grey_15 text-lg font-medium mb-5 leading-[150%]">
+                        <h3 className="text-grey_15 text-lg text-center font-medium mb-5 leading-[150%]">
                             Available Features
                         </h3>
                         <div className="flex flex-col gap-5">
-
+                            {
+                                availableFeatures.map((feature) => (
+                                    <div className="flex gap-2 border-[1px] border-white_95 p-3 rounded-md" key="">
+                                        <div className="bg-orange_95 text-grey_15 p-1 h-6 inline-block rounded-sm">
+                                            <IoMdCheckmark />
+                                        </div>
+                                        <p className="text-sm leading-[150%]">
+                                            {feature}
+                                        </p>
+                                    </div>
+                                ))
+                            }
+                            {
+                                unavailableFeatures.map((feature) => (
+                                    <div className="flex gap-2 border-[1px] border-white_95 p-3 rounded-md" key="">
+                                        <div className="text-grey_15 border-[1px] border-white_95 p-1 h-6 inline-block rounded-sm">
+                                            <FaXmark />
+                                        </div>
+                                        <p className="text-sm leading-[150%]">
+                                            {feature}
+                                        </p>
+                                    </div>
+                                ))
+                            }
                         </div>
                     </div>
                     <div className="bg-orange_50 text-center py-5 rounded-b-md">
--- src/components/home/pricesContainer/PricesContainer.tsx
+++ src/components/home/pricesContainer/PricesContainer.tsx
@@ -5,7 +5,21 @@ export default function PricesContainer() {
     return (
         <div className="mt-12 mx-4">
             <div className="bg-white p-5 rounded-xl">
-                <PricesCard />
+                <PricesCard
+                    planCategory="Free"
+                    planPrice="0"
+                    availableFeatures={[
+                        "Access to selected free courses.",
+                        "Limited course materials and resources.",
+                        "Basic community support.",
+                        "No certification upon completion.",
+                        "Ad-supported platform."
+                    ]}
+                    unavailableFeatures={[
+                        "Access to exclusive Pro Plan community forums.",
+                        "Early access to new courses and updates."
+                    ]}
+                />
             </div>
         </div>
     );
