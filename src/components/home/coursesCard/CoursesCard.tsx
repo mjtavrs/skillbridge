e2d04e466bda5dbd90cd62e13e41572cc5e9a72d
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-18 08:26:18 -0300

    feat: Import the courses into the CoursesContainer, refactor part of the CoursesCard code to make it dinamyc, receiving the info as props.

--- src/components/home/coursesCard/CoursesCard.tsx
+++ src/components/home/coursesCard/CoursesCard.tsx
@@ -1,15 +1,23 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import Link from "next/link";
 
 import Tag from "@/components/shared/tag/Tag";
 
-export default function CoursesCard() {
+type Props = {
+    cover: StaticImageData,
+    tags: Array<string>
+    author: string,
+    title: string,
+    description: string,
+}
+
+export default function CoursesCard({ cover, tags, author, title, description }: Props) {
     return (
         <div className="bg-white border-[1px] border-white_95 p-6 rounded-xl">
             <div className="flex flex-col gap-6">
                 <div className="relative h-44 w-full">
                     <Image
-                        src="/assets/png/covers/coursesCovers/webDesignCover.png"
+                        src={cover}
                         alt="Course cover"
                         layout="fill"
                         objectFit="cover"
@@ -18,23 +26,25 @@ export default function CoursesCard() {
                 </div>
                 <div>
                     <div className="flex gap-3 mb-3">
-                        <Tag
-                            title="4 Weeks"
-                        />
-                        <Tag
-                            title="Beginner"
-                        />
+                        {
+                            tags.map((tag) => (
+                                <Tag
+                                    key=""
+                                    title={tag}
+                                />
+                            ))
+                        }
                     </div>
                     <h4 className="text-grey_15 font-medium">
-                        By John Smith
+                        By {author}
                     </h4>
                 </div>
                 <div>
                     <h3 className="text-grey_15 text-lg font-semibold mb-3 leading-[150%]">
-                        Web Design Fundamentals
+                        {title}
                     </h3>
                     <p className="text-grey_30 text-sm leading-[150%]">
-                        Learn the fundamentals of web design, including HTML, CSS, and responsive design principles. Develop the skills to create visually appealing and user-friendly websites.
+                        {description}
                     </p>
                 </div>
                 <div className="bg-white_97 border-[1px] border-white_95 text-center py-3 rounded-md">
--- src/components/home/coursesContainer/CoursesContainer.tsx
+++ src/components/home/coursesContainer/CoursesContainer.tsx
@@ -1,6 +1,8 @@
 import CoursesCard from "../coursesCard/CoursesCard";
 import HomeSection from "../homeSection/HomeSection";
 
+import webDesignCover from "../../../../public/assets/png/covers/coursesCovers/webDesignCover.png";
+
 export default function CoursesContainer() {
     return (
         <div className="mt-12">
@@ -9,7 +11,41 @@ export default function CoursesContainer() {
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ratione nisi est natus quidem aliquam modi, aut ipsa similique pariatur a et id explicabo, harum vero sunt. Amet, optio necessitatibus?"
                 link="#"
             >
-                <CoursesCard />
+                <CoursesCard
+                    cover={webDesignCover}
+                    tags={["4 Weeks", "Beginner"]}
+                    author="John Smith"
+                    title="Web Design Fundamentals"
+                    description="Learn the fundamentals of web design, including HTML, CSS, and responsive design principles. Develop the skills to create visually appealing and user-friendly websites."
+                />
+                <CoursesCard
+                    cover={webDesignCover}
+                    tags={["6 Weeks", "Intermediate"]}
+                    author="Emily Johnson"
+                    title="UI/UX Design"
+                    description="Master the art of creating intuitive user interfaces (UI) and enhancing user experiences (UX). Learn design principles, wireframing, prototyping, and usability testing techniques."
+                />
+                <CoursesCard
+                    cover={webDesignCover}
+                    tags={["8 Weeks", "Intermediate"]}
+                    author="David Brown"
+                    title="Mobile App Development"
+                    description="Dive into the world of mobile app development. Learn to build native iOS and Android applications using industry-leading frameworks like Swift and Kotlin."
+                />
+                <CoursesCard
+                    cover={webDesignCover}
+                    tags={["10 Weeks", "Intermediate"]}
+                    author="Michael Adams"
+                    title="Front-End Web Development"
+                    description="Become proficient in front-end web development. Learn HTML, CSS, JavaScript, and popular frameworks like Bootstrap and React. Build interactive and responsive websites."
+                />
+                <CoursesCard
+                    cover={webDesignCover}
+                    tags={["6 Weeks", "Advanced"]}
+                    author="Jennifer Wilson"
+                    title="Advanced JavaScript"
+                    description="Take your JavaScript skills to the next level. Explore advanced concepts like closures, prototypes, asynchronous programming, and ES6 features. Build complex applications with confidence."
+                />
             </HomeSection>
         </div>
     );
