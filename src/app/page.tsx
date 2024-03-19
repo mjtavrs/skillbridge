7560a1fc88a112cb6c41306f6171fc2bbdc53049
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-19 17:05:18 -0300

    refactor: Delete the link prop from the HomeSection component inside the CoursesContainer component. Import the courses covers for use inside the CoursesContainer component.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import BenefitsContainer from "@/components/home/benefitsContainer/BenefitsConta
 import CoursesContainer from "@/components/home/coursesContainer/CoursesContainer";
 import PricesContainer from "@/components/home/pricesContainer/PricesContainer";
 import NavigationMenu from "@/components/shared/navigationMenu/NavigationMenu";
+import FaqContainer from "@/components/home/faqContainer/FaqContainer";
 import HomeVideo from "../../public/assets/png/covers/homeVideo.png";
 import Companies from "@/components/home/companies/Companies";
 import Footer from "@/components/shared/footer/Footer";
 import Video from "@/components/shared/video/Video";
 import Hero from "@/components/home/hero/Hero";
 import Cta from "@/components/shared/cta/Cta";
-import FaqContainer from "@/components/home/faqContainer/FaqContainer";
 
 export default function Home() {
   return (
--- src/components/home/coursesContainer/CoursesContainer.tsx
+++ src/components/home/coursesContainer/CoursesContainer.tsx
@@ -9,7 +9,6 @@ export default function CoursesContainer() {
             <HomeSection
                 title="Our Courses"
                 description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore ratione nisi est natus quidem aliquam modi, aut ipsa similique pariatur a et id explicabo, harum vero sunt. Amet, optio necessitatibus?"
-                link="#"
             >
                 <CoursesCard
                     cover={webDesignCover}
