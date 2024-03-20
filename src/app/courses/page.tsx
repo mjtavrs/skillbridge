89b55acb0f3ab2f71f7f7f37824f57e844f890a1
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-20 17:28:10 -0300

    feat: Finish the CoursesFullContainer component, refactor the page.tsx for the courses page and create the page.tsx for the OpenCourse area.
    The design given by Produce UI only gives info for the UI/UX Design course, so it'll be the only link working inside the courses and home pages.

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,8 +1,8 @@
 import CoursesFullContainer from "@/components/courses/coursesFullContainer/CoursesFullContainer";
-import Cta from "@/components/shared/cta/Cta";
-import Footer from "@/components/shared/footer/Footer";
 import NavigationMenu from "@/components/shared/navigationMenu/NavigationMenu";
 import PageHero from "@/components/shared/pageHero/PageHero";
+import Footer from "@/components/shared/footer/Footer";
+import Cta from "@/components/shared/cta/Cta";
 
 
 export default function Courses() {
