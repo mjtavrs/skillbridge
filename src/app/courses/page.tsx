408c8a9c2b75472a006dc7febdba6913befaab0d
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-20 06:40:53 -0300

    feat: Include the Cta, NavigationMenu and Footer components inside the Courses page.

--- src/app/courses/page.tsx
+++ src/app/courses/page.tsx
@@ -1,5 +1,14 @@
+import Cta from "@/components/shared/cta/Cta";
+import Footer from "@/components/shared/footer/Footer";
+import NavigationMenu from "@/components/shared/navigationMenu/NavigationMenu";
+
+
 export default function Courses() {
     return (
-        <p>Courses page</p>
+        <>
+            <Cta />
+            <NavigationMenu />
+            <Footer />
+        </>
     );
 }
