d2b86ecf7faf8ba53e6f9945feedd8026466be7f
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-25 08:31:10 -0300

    feat: Create the GoalsContainer and CtaCard components, import the abstract logo SVG.
    The About Us page holds a second version of a CTA, which will be a card to be shown in the end of the page and will problably take the users to the prices page. So, I decided to already create the component and submit the code in the next commits.

--- src/components/aboutUs/aboutContainer/AboutContainer.tsx
+++ src/components/aboutUs/aboutContainer/AboutContainer.tsx
@@ -1,9 +1,11 @@
 import AchievementsContainer from "../achievementsContainer/AchievementsContainer";
+import GoalsContainer from "@/components/aboutUs/goalsContainer/GoalsContainer";
 
 export default function AboutContainer() {
     return (
-        <div className="mx-4 mt-12">
+        <div className="flex flex-col gap-12 mx-4 mt-12">
             <AchievementsContainer />
+            <GoalsContainer />
         </div>
     );
 }
--- src/components/aboutUs/achievementsContainer/AchievementsContainer.tsx
+++ src/components/aboutUs/achievementsContainer/AchievementsContainer.tsx
@@ -1,20 +1,21 @@
 import AboutCard from "../aboutCard/AboutCard";
 
-import { FaMasksTheater, FaShieldHalved } from "react-icons/fa6";
 import { FaCrown, FaMedal } from "react-icons/fa";
+import { FaHandshake } from "react-icons/fa6";
+import { IoIosHappy } from "react-icons/io";
 
 export default function AchievementsContainer() {
     return (
-        <>
+        <div className="flex flex-col gap-10">
             <div className="flex flex-col gap-1">
                 <h2 className="text-2xl text-grey_20 font-medium leading-[150%]">
                     Achievements
                 </h2>
                 <p className="text-sm text-grey_35 leading-[150%]">
-                    Our commitment to excellence has led us to achieve significant milestones along our journey. Here are some of our notable achievements
+                    Our commitment to excellence has led us to achieve significant milestones along our journey. Here are some of our notable achievements.
                 </p>
             </div>
-            <div className="flex flex-col gap-5 mt-12">
+            <div className="flex flex-col gap-5">
                 <AboutCard
                     info={[
                         {
@@ -36,7 +37,7 @@ export default function AchievementsContainer() {
                 <AboutCard
                     info={[
                         {
-                            icon: < FaMasksTheater />,
+                            icon: < IoIosHappy />,
                             title: "Positive Student Feedback",
                             description: "We take pride in the positive feedback we receive from our students, who appreciate the practicality and relevance of our course materials."
                         }
@@ -45,13 +46,13 @@ export default function AchievementsContainer() {
                 <AboutCard
                     info={[
                         {
-                            icon: < FaShieldHalved />,
+                            icon: < FaHandshake />,
                             title: "Industry Partnerships",
-                            description: "We have established strong partnerships with industry leaders, enabling us to provide our students with access to the latest tools and technologies"
+                            description: "We have established strong partnerships with industry leaders, enabling us to provide our students with access to the latest tools and technologies."
                         }
                     ]}
                 />
             </div>
-        </>
+        </div>
     );
 }
