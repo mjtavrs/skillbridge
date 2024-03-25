038bff454d2af028d1620793bfbe033c3a68c8c2
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-25 08:54:36 -0300

    feat: Organize the imports into the page.tsx for the About Us and create the page.tsx for the Pricing.

--- src/app/about-us/page.tsx
+++ src/app/about-us/page.tsx
@@ -1,9 +1,9 @@
 import AboutContainer from "@/components/aboutUs/aboutContainer/AboutContainer";
 import NavigationMenu from "@/components/shared/navigationMenu/NavigationMenu";
 import PageHero from "@/components/shared/pageHero/PageHero";
+import CtaCard from "@/components/aboutUs/ctaCard/CtaCard";
 import Footer from "@/components/shared/footer/Footer";
 import Cta from "@/components/shared/cta/Cta";
-import CtaCard from "@/components/aboutUs/ctaCard/CtaCard";
 
 
 export default function AboutUs() {
