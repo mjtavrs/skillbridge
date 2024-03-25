12238be787b29f0d2bcc6787da76478f89c4dbe5
Marcos Tavares <mjtdes.md@gmail.com>
2024-03-25 11:40:28 -0300

    feat: Create the Login page and import the base components.

---
+++ src/app/login/page.tsx
@@ -0,0 +1,14 @@
+import NavigationMenu from "@/components/shared/navigationMenu/NavigationMenu";
+import Footer from "@/components/shared/footer/Footer";
+import Cta from "@/components/shared/cta/Cta";
+
+export default function LogIn() {
+    return (
+        <>
+            <Cta />
+            <NavigationMenu />
+
+            <Footer />
+        </>
+    );
+}
