a31f757ccc63263b8df49c1c330f455fda3c924c
Marcos Tavares <mjtdes.md@gmail.com>
2024-04-15 09:04:12 -0300

    refactor: Remove the framer motion usage and the dynamic use inside the MapTestimonials and TestimonialsCard components.
    After thinking a little bit more, I found the challenge is a little bit harder than I expected. So I'm gonna think a new way to dynamically use the testimonials inside other pages, giving the fact that some components could be shared but I still did not find a way to do it. Will work on this later on.

--- src/components/home/testimonialsCard/TestimonialsCard.tsx
+++ src/components/home/testimonialsCard/TestimonialsCard.tsx
@@ -5,44 +5,41 @@ type Props = {
     userTestimonial: string,
     userPicture: string,
     userFullName: string,
-    isVisible: boolean
 }
 
-export default function TestimonialsCard({ userTestimonial, userPicture, userFullName, isVisible }: Props) {
+export default function TestimonialsCard({ userTestimonial, userPicture, userFullName }: Props) {
     return (
-        <div className={isVisible ? "block" : "hidden"}>
-            <div className="flex flex-col">
-                <div className="bg-white border-[1px] border-white_95 border-b-0 p-8 rounded-t-xl">
-                    <div>
-                        <p className="text-sm text-grey_30 leading-[150%]">
-                            {userTestimonial}
-                        </p>
-                    </div>
+        <div className="flex flex-col">
+            <div className="bg-white border-[1px] border-white_95 border-b-0 p-8 rounded-t-xl">
+                <div>
+                    <p className="text-sm text-grey_30 leading-[150%]">
+                        {userTestimonial}
+                    </p>
                 </div>
-                <div className="bg-white_99 border-[1px] border-white_95 py-5 px-8 rounded-b-xl">
-                    <div className="flex items-center justify-between">
-                        <div className="flex items-center gap-3">
-                            <div className="relative h-[50px] w-[50px]">
-                                <Image
-                                    src={userPicture}
-                                    alt="User picture"
-                                    layout="fill"
-                                    objectFit="cover"
-                                    className="brightness-95 rounded-md"
-                                />
-                            </div>
-                            <h3 className="text-grey_20 font-semibold leading-[150%]">
-                                {userFullName}
-                            </h3>
+            </div>
+            <div className="bg-white_99 border-[1px] border-white_95 py-5 px-8 rounded-b-xl">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-3">
+                        <div className="relative h-[50px] w-[50px]">
+                            <Image
+                                src={userPicture}
+                                alt="User picture"
+                                layout="fill"
+                                objectFit="cover"
+                                className="brightness-95 rounded-md"
+                            />
                         </div>
-                        <div className="bg-white_97 border-[1px] border-white_95 p-4 rounded-md">
-                            <Link
-                                href="#"
-                                className="text-grey_15 text-sm font-medium leading-[150%]"
-                            >
-                                Read Full Story
-                            </Link>
-                        </div>
+                        <h3 className="text-grey_20 font-semibold leading-[150%]">
+                            {userFullName}
+                        </h3>
+                    </div>
+                    <div className="bg-white_97 border-[1px] border-white_95 p-4 rounded-md">
+                        <Link
+                            href="#"
+                            className="text-grey_15 text-sm font-medium leading-[150%]"
+                        >
+                            Read Full Story
+                        </Link>
                     </div>
                 </div>
             </div>
--- src/components/shared/testimonialsCarousel/TestimonialsCarousel.tsx
+++ src/components/shared/testimonialsCarousel/TestimonialsCarousel.tsx
@@ -1,23 +1,10 @@
 "use client";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
-import { motion } from "framer-motion";
-import { useState } from "react";
+import { AnimatePresence } from "framer-motion";
 
 import MapTestimonials from "../mapTestimonials/MapTestimonials";
-import testimonials from "@/data/testimonialsData";
 
 export default function TestimonialsCarousel() {
-
-    const [currentIndex, setCurrentIndex] = useState(0);
-
-    const nextTestimonial = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === testimonials.length - 1 ? 0 : prevIndex + 1));
-    }
-
-    const prevTestimonial = () => {
-        setCurrentIndex((prevIndex) => (prevIndex === 0 ? testimonials.length - 1 : prevIndex - 1));
-    }
-
     return (
         <div className="mt-12 mx-4">
             <div className="flex flex-col gap-1">
@@ -28,20 +15,18 @@ export default function TestimonialsCarousel() {
                     Lorem ipsum dolor sit amet consectetur. Tempus tincidunt etiam eget elit id imperdiet et. Cras eu sit dignissim lorem nibh et. Ac cum eget habitasse in velit fringilla feugiat senectus in.
                 </p>
             </div>
-            <div className="mt-10 mb-5">
-                <MapTestimonials
-                    currentIndex={currentIndex}
-                />
+            <div className="mt-10 mb-5 relative">
+                <AnimatePresence mode="wait">
+                    <MapTestimonials />
+                </AnimatePresence>
             </div>
             <div className="flex items-center justify-center gap-3 text-xl text-grey_15">
                 <div className="bg-white border-[1px] border-white_95 rounded-md p-3">
                     <FaArrowLeft
-                        onClick={prevTestimonial}
                     />
                 </div>
                 <div className="bg-white border-[1px] border-white_95 rounded-md p-3">
                     <FaArrowRight
-                        onClick={nextTestimonial}
                     />
                 </div>
             </div>
