b729b61e67fd6fb1511dd486a615fb73aeff12cb
Marcos Tavares <mjtdes.md@gmail.com>
2024-04-18 10:24:07 -0300

    feat: Create the curriculumExampleData script, which holds the data for the open course example. Refactor the open course page.tsx.

--- src/app/course/page.tsx
+++ src/app/course/page.tsx
@@ -1,15 +1,16 @@
 import CurriculumCard from "@/components/openCourse/curriculumCard/CurriculumCard";
 import NavigationMenu from "@/components/shared/navigationMenu/NavigationMenu";
+import ColOrganizer from "@/components/shared/colOrganizer/ColOrganizer";
 import PageHero from "@/components/shared/pageHero/PageHero";
 import Footer from "@/components/shared/footer/Footer";
 import Video from "@/components/shared/video/Video";
 import Cta from "@/components/shared/cta/Cta";
 
+import curriculumInfo from "@/data/curriculumExampleData";
+
 import UiUxCover from "../../../public/assets/png/covers/coursesPage/uiUxDesign/1.png";
-import ColOrganizer from "@/components/shared/colOrganizer/ColOrganizer";
 
 export default function Course() {
-
     return (
         <>
             <Cta />
@@ -25,51 +26,16 @@ export default function Course() {
             </div>
             <div className="flex flex-col gap-5 mx-4 laptop:mx-20">
                 <ColOrganizer colNumber={2}>
-                    <CurriculumCard
-                        moduleNumber="01"
-                        moduleTitle="Introduction to UI/UX Design"
-                        moduleClasses={[
-                            { title: "Understanding UI/UX Design Principles", duration: "45 Minutes" },
-                            { title: "Importance of User-Centered Design", duration: "1 Hour" },
-                            { title: "The Role of UI/UX Design in Product Development", duration: "45 minutes" }
-                        ]}
-                    />
-                    <CurriculumCard
-                        moduleNumber="02"
-                        moduleTitle="User Research and Analysis"
-                        moduleClasses={[
-                            { title: "Conducting User Research and Interviews", duration: "1 Hour" },
-                            { title: "Analyzing User Needs and Behavior", duration: "1 Hour" },
-                            { title: "Creating User Personas and Scenarios", duration: "45 minutes" }
-                        ]}
-                    />
-                    <CurriculumCard
-                        moduleNumber="03"
-                        moduleTitle="Wireframing and Prototyping"
-                        moduleClasses={[
-                            { title: "Introduction to Wireframing Tools and Techniques", duration: "1 Hour" },
-                            { title: "Creating Low-Fidelity Wireframes", duration: "1 Hour" },
-                            { title: "Prototyping and Interactive Mockups", duration: "1 Hour" }
-                        ]}
-                    />
-                    <CurriculumCard
-                        moduleNumber="04"
-                        moduleTitle="Visual Design and Branding"
-                        moduleClasses={[
-                            { title: "Color Theory and Typography in UI Design", duration: "1 Hour" },
-                            { title: "Visual Hierarchy and Layout Design", duration: "1 Hour" },
-                            { title: "Creating a Strong Brand Identity", duration: "45 Minutes" }
-                        ]}
-                    />
-                    <CurriculumCard
-                        moduleNumber="05"
-                        moduleTitle="Usability Testing and Iteration"
-                        moduleClasses={[
-                            { title: "Usability Testing Methods and Techniques", duration: "1 Hour" },
-                            { title: "Analyzing Usability Test Results", duration: "45 Minutes" },
-                            { title: "Iterating and Improving UX Designs", duration: "45 Minutes" }
-                        ]}
-                    />
+                    {
+                        curriculumInfo.map((curriculum, index) => (
+                            <CurriculumCard
+                                key={index}
+                                moduleNumber={curriculum.classNumber}
+                                moduleTitle={curriculum.classTitle}
+                                moduleClasses={curriculum.classes}
+                            />
+                        ))
+                    }
                 </ColOrganizer>
             </div>
             <Footer />
---
+++ src/data/curriculumExampleData.js
@@ -0,0 +1,49 @@
+const curriculumInfo = [
+    {
+        classNumber: "01",
+        classTitle: "Introduction to UI/UX Design",
+        classes: [
+            { title: "Understanding UI/UX Design Principles", duration: "45 Minutes" },
+            { title: "Importance of User-Centered Design", duration: "1 Hour" },
+            { title: "The Role of UI/UX Design in Product Development", duration: "45 Minutes" }
+        ]
+    },
+    {
+        classNumber: "02",
+        classTitle: "User Research and Analysis",
+        classes: [
+            { title: "Conducting User Research and Interviews", duration: "1 Hour" },
+            { title: "Analyzing User Needs and Behavior", duration: "1 Hour" },
+            { title: "Creating User Personas and Scenarios", duration: "45 Minutes" }
+        ]
+    },
+    {
+        classNumber: "03",
+        classTitle: "Wireframing and Prototyping",
+        classes: [
+            { title: "Introduction to Wireframing Tools and Techniques", duration: "1 Hour" },
+            { title: "Creating Low-Fidelity Wireframes", duration: "1 Hour", duration: "1 Hour" },
+            { title: "Prototyping and Interactive Mockups", duration: "1 Hour" }
+        ]
+    },
+    {
+        classNumber: "04",
+        classTitle: "Visual Design and Branding",
+        classes: [
+            { title: "Color Theory and Typography in UI Design", duration: "1 Hour" },
+            { title: "Visual Hierarchy and Layout Design", duration: "1 Hour" },
+            { title: "Creating a Strong Brand Identity", duration: "45 Minutes" }
+        ]
+    },
+    {
+        classNumber: "05",
+        classTitle: "Usability Testing and Iteration",
+        classes: [
+            { title: "Usability Testing Methods and Techniques", duration: "1 Hour" },
+            { title: "Analyzing Usability Test Results", duration: "45 Minutes" },
+            { title: "Iterating and Improving UX Designs", duration: "45 Minutes" }
+        ]
+    }
+];
+
+export default curriculumInfo;
