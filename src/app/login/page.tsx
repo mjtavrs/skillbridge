3b74e5c4783df687803ec5a3b1aca54da17d3792
Marcos Tavares <mjtdes.md@gmail.com>
2024-04-29 10:32:04 -0300

    feat: Organize the Login page for the use between the mobile and laptop versions. Will create a component that will hold better the logic and will allow the usage inside the Signup page too.

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -9,8 +9,14 @@ export default function LogIn() {
         <>
             <Cta />
             <NavigationMenu />
-            <LoginForm />
-            <TestimonialsCarousel />
+            <div className="flex flex-col laptop:flex-row laptop:gap-20 laptop:mx-20 laptop:items-center">
+                <div className="laptop:order-last laptop:flex-1">
+                    <LoginForm />
+                </div>
+                <div className="laptop:flex-1">
+                    <TestimonialsCarousel />
+                </div>
+            </div>
             <Footer />
         </>
     );
